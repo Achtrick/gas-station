304614371e576f9bd4cf90513a40a8ec3d28e855
achtrick <achrefmtir7@gmail.com>
2024-12-08 20:05:49 +0100

    removed unused reducer

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
 import auth from "./reducers/auth";
-import ui from "./reducers/ui";
 
 // creating store
-export const store = configureStore({ reducer: { auth, ui } });
+export const store = configureStore({ reducer: { auth } });
 
 // assigning store to next wrapper
 const makeStore = () => store;
