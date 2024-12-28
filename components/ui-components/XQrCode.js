71e1e5251fc460b791e45b71ca7709dc6400c489
achtrick <achrefmtir7@gmail.com>
2024-12-28 17:18:03 +0100

    fixed qrcode reader

--- components/ui-components/XQrCode.js
+++ components/ui-components/XQrCode.js
@@ -8,6 +8,50 @@ function XQrCode({ closeAction, onSuccess }) {
   const [stream, setStream] = useState(null);
   const [isFlashlightOn, setIsFlashlightOn] = useState(false);
 
+  var html5QrCode;
+
+  useEffect(() => {
+    Html5Qrcode.getCameras().then((devices) => {
+      if (devices && devices.length) {
+        setCameraId(devices[devices.length - 1].id);
+      }
+    });
+
+    return () => {
+      if (isFlashlightOn) {
+        toggleFlashlight();
+      }
+      html5QrCode?.stop();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!!cameraId) {
+      html5QrCode = new Html5Qrcode("reader");
+      html5QrCode.start(
+        { facingMode: "environment" },
+        {
+          fps: 60,
+          qrbox: { width: 250, height: 250 },
+        },
+        async (qrCode) => {
+          await html5QrCode.getRunningTrackCapabilities();
+          if (html5QrCode.setTorchState) {
+            await html5QrCode.setTorchState(false);
+          }
+          onSuccess(qrCode);
+          html5QrCode.stop();
+        },
+        async () => {
+          await html5QrCode.getRunningTrackCapabilities();
+          if (html5QrCode.setTorchState) {
+            await html5QrCode.setTorchState(true);
+          }
+        }
+      );
+    }
+  }, [cameraId]);
+
   const toggleFlashlight = async () => {
     try {
       if (!stream) {
@@ -35,53 +79,11 @@ function XQrCode({ closeAction, onSuccess }) {
         setIsFlashlightOn(false);
       }
     } catch (err) {
-      alert("Error toggling flashlight:", JSON.stringify(err));
       alert(
         "Failed to toggle flashlight. Make sure you're using a supported device."
       );
     }
   };
-
-  var html5QrCode;
-
-  useEffect(() => {
-    Html5Qrcode.getCameras().then((devices) => {
-      if (devices && devices.length) {
-        alert(JSON.stringify(devices));
-        setCameraId(devices[devices.length - 1].id);
-      }
-    });
-  }, []);
-
-  useEffect(() => {
-    const reader = document.getElementById("reader");
-    reader.style.width = "100%";
-    reader.style.height = "100%";
-  }, []);
-
-  useEffect(() => {
-    if (!!cameraId) {
-      html5QrCode = new Html5Qrcode("reader");
-      html5QrCode.start(
-        cameraId,
-        null,
-        async (qrCode) => {
-          await html5QrCode.getRunningTrackCapabilities();
-          if (html5QrCode.setTorchState) {
-            await html5QrCode.setTorchState(false);
-          }
-          onSuccess(qrCode);
-          html5QrCode.stop();
-        },
-        async () => {
-          await html5QrCode.getRunningTrackCapabilities();
-          if (html5QrCode.setTorchState) {
-            await html5QrCode.setTorchState(true);
-          }
-        }
-      );
-    }
-  }, [cameraId]);
 
   return (
     <div className={styles.container}>
@@ -89,23 +91,21 @@ function XQrCode({ closeAction, onSuccess }) {
         <div className={styles.overlay}></div>
         <div id="reader" className={styles.reader}></div>
       </div>
-      <div className={styles.close}>
+      <div className={styles.actions}>
         <IconButton
           onClick={() => {
-            html5QrCode.stop();
+            html5QrCode?.stop();
             closeAction();
           }}
           color="black"
         >
           <Close />
         </IconButton>
-      </div>
-      <div className={styles.close}>
         <IconButton
           onClick={() => {
             toggleFlashlight();
           }}
-          color="black"
+          style={{ color: isFlashlightOn ? "orange" : "black" }}
         >
           <Light />
         </IconButton>
