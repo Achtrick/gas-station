1b1ed639778c43e3eb8cfdca25fc84c23f8be48d
achtrick <achrefmtir7@gmail.com>
2024-12-29 15:24:23 +0100

    fix flashlight cleanup

--- components/ui-components/XQrCode.js
+++ components/ui-components/XQrCode.js
@@ -5,7 +5,6 @@ import { Html5Qrcode } from "html5-qrcode";
 import { useEffect, useState } from "react";
 function XQrCode({ closeAction, onSuccess }) {
   const [cameraId, setCameraId] = useState("");
-  const [stream, setStream] = useState(null);
   const [isFlashlightOn, setIsFlashlightOn] = useState(false);
 
   var html5QrCode;
@@ -16,74 +15,34 @@ function XQrCode({ closeAction, onSuccess }) {
         setCameraId(devices[devices.length - 1].id);
       }
     });
-
-    return async () => {
-      if (isFlashlightOn) {
-        await toggleFlashlight();
-        html5QrCode?.stop();
-      }
-    };
   }, []);
 
   useEffect(() => {
     if (!!cameraId) {
       html5QrCode = new Html5Qrcode("reader");
-      html5QrCode
-        .start(
-          { facingMode: "environment" },
-          {
-            fps: 60,
-            qrbox: { width: 250, height: 250 },
-          },
-          (qrCode) => {
-            onSuccess(qrCode);
-          }
-        )
-        .then(async () => {
-          if (isFlashlightOn) {
-            await toggleFlashlight();
-          }
-          html5QrCode?.stop();
-        });
+      html5QrCode.start(
+        { facingMode: "environment" },
+        {
+          fps: 60,
+          qrbox: { width: 250, height: 250 },
+          videoConstraints: { advanced: [{ torch: true }] },
+        },
+        async (qrCode) => {
+          onSuccess(qrCode);
+          html5QrCode.stop();
+        }
+      );
     }
   }, [cameraId]);
 
   const toggleFlashlight = async () => {
-    try {
-      if (!stream) {
-        // Start accessing the camera
-        const mediaStream = await navigator.mediaDevices.getUserMedia({
-          video: { facingMode: "environment" }, // Use the rear camera
-        });
-        setStream(mediaStream);
-
-        const track = mediaStream.getVideoTracks()[0];
-        const capabilities = track.getCapabilities();
-
-        if (capabilities.torch) {
-          await track.applyConstraints({ advanced: [{ torch: true }] });
-          setIsFlashlightOn(true);
-        } else {
-          alert("Flashlight is not supported on this device.");
-        }
-      } else {
-        // Turn off the flashlight
-        const track = stream.getVideoTracks()[0];
-        await track.applyConstraints({ advanced: [{ torch: false }] });
-        track.stop(); // Stop the camera
-        setStream(null);
-        setIsFlashlightOn(false);
-      }
-    } catch (err) {
-      alert(
-        "Failed to toggle flashlight. Make sure you're using a supported device."
-      );
-    }
+    await html5QrCode.applyVideoConstraints({ advanced: [{ torch: false }] });
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.videoContainer}>
+        <div className={styles.overlay}></div>
         <div id="reader" className={styles.reader}></div>
       </div>
       <div className={styles.actions}>
